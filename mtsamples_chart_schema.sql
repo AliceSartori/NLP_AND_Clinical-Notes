ac880d0f00845d9ae57a22e14e161baa3880afe2
dramane188 <72475690+dramane188@users.noreply.github.com>
2021-04-24 02:40:57 -0400

    mtsamples_chart_schema.sql

---
+++ mtsamples_chart_schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE mtsamples (
+id Serial PRIMARY KEY,
+description VARCHAR,
+medical_specialty VARCHAR,
+sample_name VARCHAR,
+transcription VARCHAR,
+keywords VARCHAR,
+tokenized_sents VARCHAR
+);
