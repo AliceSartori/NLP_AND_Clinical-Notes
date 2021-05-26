847aefc7325fa4d63351fb57b75d9c7df9fa4e2d
Alice <>
2021-05-26 12:06:03 -0400

    added files for presentation

--- mtsamples_chart_schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE mtsamples (
-id Serial PRIMARY KEY,
-description VARCHAR,
-medical_specialty VARCHAR,
-sample_name VARCHAR,
-transcription VARCHAR,
-keywords VARCHAR,
-tokenized_sents VARCHAR
-);
